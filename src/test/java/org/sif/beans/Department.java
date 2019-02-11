d8cb4f78be12c0e566d26308bf622477f271f063
Carlos Eugenio <carloseugenio@gmail.com>
2019-02-11 08:59:52 -0300

    Added model testes

---
+++ src/test/java/org/sif/beans/Department.java
@@ -0,0 +1,77 @@
+package org.sif.beans;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Department implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Version
+	private Long versionNumber;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@OneToMany(targetEntity = Employee.class)
+	private Set<Employee> employeelist = new HashSet<>();
+
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean equals(Object other) {
+		if (other == null) {
+			return false;
+		}
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof Department)) {
+			return false;
+		}
+
+		final Department department = (Department) other;
+
+		return new EqualsBuilder()
+				.appendSuper(super.equals(other))
+				.append(name, department.name)
+				.isEquals();
+	}
+
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).
+				appendSuper(37).
+				append(name).
+				toHashCode();
+	}
+
+	public Set<Employee> getEmployeelist() {
+		return employeelist;
+	}
+
+	public void setEmployeelist(Set<Employee> employeelist) {
+		this.employeelist = employeelist;
+	}
+}
---
+++ src/test/java/org/sif/beans/Employee.java
@@ -0,0 +1,105 @@
+package org.sif.beans;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(
+			name="department",
+			referencedColumnName="id",
+			insertable=true,
+			updatable=true,
+			nullable=true,
+			unique=false)
+	private Department department;
+
+	@Basic(optional = true)
+	// The column for this attribute
+	@Column(
+			name="name",
+			insertable=true,
+			updatable=true,
+			nullable=true,
+			unique=false)
+	private String name;
+
+	@Basic(optional = true)
+	@Column(name = "age", insertable = true, updatable = true, nullable = true, unique = false)
+	/** Declaration of Attribute idade */
+	private Integer age;
+
+
+	@OneToMany(targetEntity=Employee.class)
+	private Set<Employee> friends = new HashSet<>();
+
+	@OneToMany(targetEntity=Department.class)
+	private Set<Department> coordinator = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+
+	public Set<Employee> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(Set<Employee> friends) {
+		this.friends = friends;
+	}
+
+	public Set<Department> getCoordinator() {
+		return coordinator;
+	}
+
+	public void setCoordinator(Set<Department> coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
